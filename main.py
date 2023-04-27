9d9b357693337f2dffe5d46897b182f66732f8d4
NISHIUMI Keisuke <nishiumi-keisuke@kino3.jp>
2023-04-27 11:53:03 +0900

    テキストをベクトル化する

--- main.py
+++ main.py
@@ -1,8 +1,39 @@
 import tensorflow as tf
 
+# シェイクスピアのデータセットをダウンロード
 path_to_file = tf.keras.utils.get_file(
     'shakespeare.txt',
     'https://storage.googleapis.com/download.tensorflow.org/data/shakespeare.txt'
 )
 text = open(path_to_file, 'rb').read().decode(encoding='utf-8')
-print(f'Length of text: {len(text)} characters')
+# print(f'Length of text: {len(text)} characters')
+# print(text[:250])
+
+# ファイルのuniqueな文字
+vocab = sorted(set(text))
+# print(f'{len(vocab)} unique charactors')
+
+
+example_texts = ['abcdefg', 'xyz']
+
+# textをtokenに分割する
+chars = tf.strings.unicode_split(example_texts, input_encoding='UTF-8')
+
+# StringLookupレイヤー作成
+ids_from_chars = tf.keras.layers.StringLookup(
+    vocabulary=list(vocab), mask_token=None
+)
+ids = ids_from_chars(chars)
+# print(ids)
+
+chars_from_ids = tf.keras.layers.StringLookup(
+    vocabulary=ids_from_chars.get_vocabulary(), invert=True, mask_token=None
+)
+chars = chars_from_ids(ids)
+# print(chars)
+
+def text_from_ids(ids):
+    return tf.strings.reduce_join(chars_from_ids(ids), axis=-1)
+
+print(text_from_ids(ids))
+print(text_from_ids(ids).numpy())
