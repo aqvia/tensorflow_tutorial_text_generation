bc71d54e7b81c662e08d2dc32814117454294277
NISHIUMI Keisuke <nishiumi-keisuke@kino3.jp>
2023-04-27 12:02:29 +0900

    テキストをシーケンスに分割

--- main.py
+++ main.py
@@ -35,5 +35,19 @@ chars = chars_from_ids(ids)
 def text_from_ids(ids):
     return tf.strings.reduce_join(chars_from_ids(ids), axis=-1)
 
-print(text_from_ids(ids))
-print(text_from_ids(ids).numpy())
+# print(text_from_ids(ids))
+# print(text_from_ids(ids).numpy())
+
+
+all_ids = ids_from_chars(tf.strings.unicode_split(text, 'UTF-8'))
+ids_dataset = tf.data.Dataset.from_tensor_slices(all_ids)
+# for ids in ids_dataset.take(10):
+#     print(chars_from_ids(ids).numpy().decode('utf-8'))
+
+seq_length = 100
+example_per_epoch = len(text)//(seq_length + 1)
+
+sequences = ids_dataset.batch(seq_length + 1, drop_remainder=True)
+
+for seq in sequences.take(5):
+    print(text_from_ids(seq).numpy())
